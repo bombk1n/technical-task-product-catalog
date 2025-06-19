2db9d450060e43c29a9936f6aea74e87559ca222
bombk1nKPI <grinenko.roman@lll.kpi.ua>
2025-06-19 21:18:22 +0300

    chore: add Caffeine config and dependency in pom

--- src/main/java/com/bombk1n/technicaltaskproductcatalog/TechnicalTaskProductCatalogApplication.java
+++ src/main/java/com/bombk1n/technicaltaskproductcatalog/TechnicalTaskProductCatalogApplication.java
@@ -2,8 +2,10 @@ package com.bombk1n.technicaltaskproductcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TechnicalTaskProductCatalogApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/bombk1n/technicaltaskproductcatalog/config/CacheConfig.java
@@ -0,0 +1,33 @@
+package com.bombk1n.technicaltaskproductcatalog.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .initialCapacity(50)
+                .maximumSize(500)
+                .recordStats();
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager(
+                "productCache",
+                "categoryProductsCache"
+        );
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+}
