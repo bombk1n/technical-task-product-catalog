dc317c7c0d822834dac427898b60408ae79d7ddc
bombk1nKPI <grinenko.roman@lll.kpi.ua>
2025-06-19 21:14:00 +0300

    chore: add ModelMapperConfig and dependency in pom

---
+++ src/main/java/com/bombk1n/technicaltaskproductcatalog/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.bombk1n.technicaltaskproductcatalog.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
