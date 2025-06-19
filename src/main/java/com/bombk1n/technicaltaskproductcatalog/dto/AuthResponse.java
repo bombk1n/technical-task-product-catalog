6efc70ce9e19045dde30a2065e45f05040257506
bombk1nKPI <grinenko.roman@lll.kpi.ua>
2025-06-20 01:10:35 +0300

    feat: add dtos AuthRequest and AuthResponse

---
+++ src/main/java/com/bombk1n/technicaltaskproductcatalog/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.bombk1n.technicaltaskproductcatalog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
