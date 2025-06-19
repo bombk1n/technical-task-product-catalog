9526ad01b6f0599fa320e3cd7666859da961393e
bombk1nKPI <grinenko.roman@lll.kpi.ua>
2025-06-19 14:25:51 +0300

    feat: add ProductRepository with findByCategory methods

---
+++ src/main/java/com/bombk1n/technicaltaskproductcatalog/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.bombk1n.technicaltaskproductcatalog.repository;
+
+import com.bombk1n.technicaltaskproductcatalog.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategory(String category, Pageable pageable);
+
+    List<Product> findByCategory(String category);
+}
