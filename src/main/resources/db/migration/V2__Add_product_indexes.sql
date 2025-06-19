465912d28b47fc7d2387860c91a6b0fca8619fe3
bombk1nKPI <grinenko.roman@lll.kpi.ua>
2025-06-19 14:10:28 +0300

    feat: add h2 db connectivity and flyway scripts for creating product table and its scripts

---
+++ src/main/resources/db/migration/V2__Add_product_indexes.sql
@@ -0,0 +1,7 @@
+CREATE INDEX idx_product_name ON products(name);
+
+CREATE INDEX idx_product_category ON products(category);
+
+CREATE INDEX idx_product_price ON products(price);
+
+CREATE INDEX idx_product_created_date ON products(created_date);
